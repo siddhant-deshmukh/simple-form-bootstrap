3a17e85d4249920a9cd139d4d75cf69733287c4e
siddhant-deshmukh <siddhant946044@gmail.com>
2024-02-03 19:53:59 +0530

    .

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,3 +1,4 @@
+import { Button } from "react-bootstrap"
 import { IUser } from "../App"
 import TableRow from "./TableRow"
 
@@ -37,28 +38,47 @@ export default function Table({ setUser }: {
 
   return (
     <div>
-      <div className="relative max-w-5xl pt-5 mx-auto overflow-x-auto shadow-md sm:rounded-lg">
-        <div className="flex items-center justify-between flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white dark:bg-gray-900">
+      <div
+        style={{
+          maxWidth: "1024px",
+          paddingTop: "20px",
+          overflowX: "auto"
+        }}
+        className="relative mx-auto">
+        <div className="d-flex align-items-center justify-content-between flex-column flex-wrap flex-md-row p-4 bg-white">
           <div>
-            <button
-              onClick={() => { Logout() }}
-              id="dropdownActionButton" data-dropdown-toggle="dropdownAction" className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
-              <span className="sr-only">Logout button</span>
-              Logout
-            </button>
+            <Button onClick={() => { Logout() }} >Logout</Button>
           </div>
-          <label htmlFor="table-search" className="sr-only">Search</label>
-          <div className="relative">
-            <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-              <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+          <div className="position-relative">
+            <div
+              style={{
+                top: "0px",
+                bottom: "0px",
+                pointerEvents: "none",
+                left: "10px"
+              }}
+              className="position-absolute d-flex align-items-center">
+              <svg width={20} height={20} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
               </svg>
             </div>
-            <input type="text" id="table-search-users" className="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for users" />
+            <input
+              type="text"
+              id="table-search-users"
+              style={{
+                width: "320px",
+                display: "block",
+                paddingLeft: "40px",
+                // padding: "8px",
+                // paddingInlineStart: "50px",
+                // border: "1px solid rgb(209 213 219)",
+                backgroundColor: "rgb(249 250 251)"
+              }}
+              className="py-2 outlin-none text-sm" placeholder="Search for users" />
           </div>
         </div>
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <table id="the_table" className="w-100 text-sm text-left ">
+          <thead  className="text-xs ">
             <tr>
               <th scope="col" className="px-6 py-3">
                 Name
@@ -79,7 +99,7 @@ export default function Table({ setUser }: {
             {
               data.map((user) => {
                 // console.log(imgUrl)
-                return <TableRow user={user}/>
+                return <TableRow user={user} />
               })
             }
           </tbody>
--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -8,26 +8,43 @@ export default function TableRow({ user }: {
   const imgUrl = faker.image.urlLoremFlickr({ category: 'cats' })
   const randomNum = Math.random()
   return (
-    <tr key={user._id + user.email} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-      <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+    <tr
+      style={{
+        borderBottom: "1px solid lightgray",
+      }}
+      key={user._id + user.email} className="bg-white  hover:bg-gray-50">
+      <th scope="row" style={{ whiteSpace: "nowrap" }} className="d-flex align-items-center   ">
         <img
-          className="w-10 h-10 rounded-full"
+          width={40}
+          height={40}
+          style={{
+            borderRadius: "9999px"
+          }}
           src={imgUrl} alt="Jese image" />
         <div className="ps-3">
-          <div className="text-base font-semibold">{user.name}</div>
-          <div className="font-normal text-gray-500">{user.email}</div>
+          <div style={{ fontWeight: 600 }} >{user.name}</div>
+          <div style={{ fontWeight: 400, color: "rgb(107 114 128)" }}>{user.email}</div>
         </div>
       </th>
-      <td className="px-6 py-4">
+      <td className="">
         {date.toDateString()}
       </td>
-      <td className="px-6 py-4">
-        <div className="flex items-center">
-          <div className={`h-2.5 w-2.5 rounded-full ${randomNum > 0.5 ? 'bg-green-500' : 'bg-red-500'} me-2`}></div> Online
+      <td className="">
+        <div className="d-flex align-items-center">
+          <div
+            style={{
+              height: "10px",
+              width: "10px",
+              borderRadius: "100px",
+              marginRight: "5px",
+              backgroundColor: randomNum > 0.5 ? 'rgb(34 197 94)' : 'rgb(239 68 68)'
+            }}
+            className={`rounded-full`}></div>
+          Online
         </div>
       </td>
-      <td className="px-6 py-4">
-        <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit user</a>
+      <td className="">
+        <a href="#" style={{ fontWeight: 500 }} className="underline-hover">Edit user</a>
       </td>
     </tr>
   )
